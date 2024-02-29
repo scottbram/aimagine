d2b96931241f8fd295944585643ebbb670ad20a9
Scott Bram <1327149+scottbram@users.noreply.github.com>
2024-02-29 11:29:47 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * - Get holiday API
+ * - Filter holiday names
+ * - Randomly select a holiday from resulting array
+ * - Send text as prompt, to incl. holiday text *in* image
+ * - Return resulting image URL
+ */
+
