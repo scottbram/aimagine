f5d1b1d8c1f02cc4b919761f0c18ab704c974b93
Scott Bram <1327149+scottbram@users.noreply.github.com>
2024-02-29 21:49:06 +0000

    Trying more stuff, pt. eleventeen

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
-    
-    
+    // eleventyConfig.addPassthroughCopy("./src/index.html");
+    // eleventyConfig.addPassthroughCopy("./src/css");
+    // eleventyConfig.addPassthroughCopy("./src/js");
+    // eleventyConfig.addPassthroughCopy("./src/img");
+    passthroughFileCopy: true;
+
     return {
         dir: {
             input: "./src",
