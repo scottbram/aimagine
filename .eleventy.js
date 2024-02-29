515c7ed6535a0acf578c4c74dbd68c8b4af7e689
Scott Bram <1327149+scottbram@users.noreply.github.com>
2024-02-29 21:44:23 +0000

    Let us try this

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/index.html");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/img");
+    
     
     return {
         dir: {
